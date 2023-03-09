9ab52b2688030b1dc5aca90dec3106d2c7ea128a
maximiliano019 <112456526+maximiliano019@users.noreply.github.com>
2023-03-09 01:02:55 -0300

    ajustes en css
    ajustes en css

--- js/menu.js
+++ js/menu.js
@@ -1,19 +1,19 @@
 function ejecutar (id){  
     
     var intro = document.getElementById('informacionPersonal');
-    intro.style.cssText = 'visibility: hidden;';
+    intro.style.cssText = 'visibility: hidden; position: relative; height: 1px;';
 
     var intro = document.getElementById('ExperienciaLaboral');
-    intro.style.cssText = 'visibility: hidden;';
+    intro.style.cssText = 'visibility: hidden; position: relative; height: 1px;';
 
     var intro = document.getElementById('Formacion');
-    intro.style.cssText = 'visibility: hidden;';
+    intro.style.cssText = 'visibility: hidden; position: relative; height: 1px;';
 
     var intro = document.getElementById('Cursos');
-    intro.style.cssText = 'visibility: hidden;';
+    intro.style.cssText = 'visibility: hidden; position: relative; height: 1px;';
 
     var intro = document.getElementById('Habilidades');
-    intro.style.cssText = 'visibility: hidden;';
+    intro.style.cssText = 'visibility: hidden; position: relative; height: 1px;';
 
     var intro = document.getElementById(id);
     intro.style.cssText = 'visibility: visible;';
